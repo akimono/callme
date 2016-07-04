c55ba8a7eab3a50af2adc25f0756cf5bf0268231
Frank Cioppettini <frank.cioppettini@gmail.com>
2016-07-04 08:55:21 -0400

    first heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,13 @@ config :callme, Callme.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
+ config :callme, Callme.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 # Do not print debug messages in production
 config :logger, level: :info
 
