3aac63727e393d1f3a03bd8ae909c9fff2fc85a1
Frank Cioppettini <frank.cioppettini@gmail.com>
2016-07-26 08:56:13 -0400

    move key

--- config/config.secret.exs
+++ config/config.secret.exs
@@ -3,3 +3,12 @@ use Mix.Config
 config :callmemd,
        mailgun_domain: "https://callmemd.com",
        mailgun_key: "key-0aac767d8bc24b174c9e3272f0ec700d"
+ config :ex_aws,
+      access_key_id: "AKIAIVCOAPOOIW5EB4FA",
+      secret_access_key: "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",
+        region: "us-west-2",
+  s3: [
+    scheme: "https://",
+    host: "s3.amazonaws.com",
+    region: "us-west-2"
+  ]
