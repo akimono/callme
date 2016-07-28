8a83a61978a3b50c8a511a501a5d4a4cd81f86a4
Frank Cioppettini <frank.cioppettini@gmail.com>
2016-07-28 14:44:30 -0400

    missing comma

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :arc,
   bucket: "callme"
 config :ex_aws,
 access_key_id: [{:system, "AWS_ACCESS_KEY"}, :instance_role],
-secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
+secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
 region: "us-west-2",
   s3: [
     scheme: "https://",
