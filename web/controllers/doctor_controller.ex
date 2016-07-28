70e9f0ab7daafe77a29c8e1c7ba1f66634e0df93
Frank Cioppettini <frank.cioppettini@gmail.com>
2016-07-28 15:26:55 -0400

    change controller for scope

--- web/controllers/doctor_controller.ex
+++ web/controllers/doctor_controller.ex
@@ -18,8 +18,8 @@ defmodule Callme.DoctorController do
   def create(conn, %{"doctor" => doctor_params}) do
     changeset = Doctor.changeset(%Doctor{}, doctor_params)
     avatar = doctor_params["avatar"]
-    scope = Repo.get(Doctor, doctor_params["id"])
-    Callme.Avatar.store(%Plug.Upload{filename: avatar.filename, path: avatar.path}, scope)
+    doctor = doctor_params["id"]
+    Callme.Avatar.store(%Plug.Upload{filename: avatar.filename, path: avatar.path}, doctor)
     case Repo.insert(changeset) do
       {:ok, _doctor} ->
         conn
