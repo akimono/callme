274f7a9d7732e4398de04678ef5c1cb2b80ec162
Frank Cioppettini <frank.cioppettini@gmail.com>
2016-07-28 15:10:24 -0400

    change avatar.ex

--- web/uploaders/avatar.ex
+++ web/uploaders/avatar.ex
@@ -4,48 +4,30 @@ defmodule Callme.Avatar do
   # Include ecto support (requires package arc_ecto installed):
   # use Arc.Ecto.Definition
 
-  @versions [:original, :thumb]
-
-
-
-   def __storage, do: Arc.Storage.Local
-
-   def filename(version,  {file, scope}), do: "#{version}-#{file.file_name}"
-
-  # To add a thumbnail version:
-  # @versions [:original, :thumb]
+    @versions [:original]
+  @extension_whitelist ~w(.jpg .jpeg .png)
 
   # Whitelist file extensions:
-  # def validate({file, _}) do
-  #   ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
-  # end
+  def validate({file, _}) do
+    # TODO: validate size
+    file_extension =
+      file.file_name
+      |> Path.extname
+      |> String.downcase
+    Enum.member?(@extension_whitelist, file_extension)
+  end
 
-  # Define a thumbnail transformation:
-  # def transform(:thumb, _) do
-  #   {:convert, "-strip -thumbnail 250x250^ -gravity center -extent 250x250 -format png", :png}
-  # end
-
-  # Override the persisted filenames:
-  # def filename(version, _) do
-  #   version
-  # end
-
-  # Override the storage directory:
-  # def storage_dir(version, {file, scope}) do
-  #   "uploads/user/avatars/#{scope.id}"
-  # end
+  def storage_dir(_version, {_file, scope}) do
+    "#{Mix.env}/uploads/avatars/#{scope.slug}"
+  end
 
   # Provide a default URL if there hasn't been a file uploaded
-  # def default_url(version, scope) do
-  #   "/images/avatars/default_#{version}.png"
-  # end
+  def default_url(_version, _scope) do
+    "http://placehold.it/200x200"
+  end
 
-  # Specify custom headers for s3 objects
-  # Available options are [:cache_control, :content_disposition,
-  #    :content_encoding, :content_length, :content_type,
-  #    :expect, :expires, :storage_class, :website_redirect_location]
-  #
-  # def s3_object_headers(version, {file, scope}) do
-  #   [content_type: Plug.MIME.path(file.file_name)]
-  # end
+  def s3_object_headers(_version, {file, _scope}) do
+    # For "image.png", would produce: "image/png"
+    [content_type: Plug.MIME.path(file.file_name)]
+  end
 end
