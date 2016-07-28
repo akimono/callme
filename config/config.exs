4b02d044edbe01598a05a91c82014760b7e6ab42
Frank Cioppettini <frank.cioppettini@gmail.com>
2016-07-28 12:39:14 -0400

    stupid error

--- config/config.exs
+++ config/config.exs
@@ -22,7 +22,7 @@ config :logger, :console,
 config :arc,
   bucket: "callme"
 config :ex_aws,
-access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+access_key_id: [{:system, "AWS_ACCESS_KEY"}, :instance_role],
 secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
 
  
