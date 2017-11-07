dad573fcb1535266d5fca2484e9bb1d711bbe3ad
Frank Cioppettini <frank.cioppettini@gmail.com>
2017-11-07 16:42:13 -0500

    undo

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Callme.Mixfile do
   def project do
     [app: :callme,
      version: "0.0.1",
-     elixir: "1.2.4",
+     elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
